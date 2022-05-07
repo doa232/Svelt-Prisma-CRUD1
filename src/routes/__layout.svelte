5df6bdb53694bb50a82534903d59d62b277053f5
R.A <doadoadaodao5@gmail.com>
2022-05-07 09:11:24 +0000

    Apply CSS styles

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<!-- A magic file to put all the css template -->
+<script>
+    import "../app.css";
+</script>
+
+<main>
+    <!-- render the content of the page -->
+    <slot />
+</main>
+<footer>
+    <!-- Now every page will have a copy right foote -->
+    Copyright
+</footer>
+ 
+
